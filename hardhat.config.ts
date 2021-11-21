ec137d19c3a6e43a16b3237f0c1b51f845434e66
gasper <gasper@sushi.com>
2021-11-22 00:45:03 +0100

    add disperse script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -163,17 +163,13 @@ const config: HardhatUserConfig = {
     ),
   },
   solidity: {
-    version: "0.6.12",
+    version: "0.8.7",
     settings: {
       optimizer: {
         enabled: true,
         runs: 200,
       },
     },
-  },
-  tenderly: {
-    project: process.env.TENDERLY_PROJECT,
-    username: process.env.TENDERLY_USERNAME,
   },
   watcher: {
     compile: {
